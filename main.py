4e47aea4209fb3a3af81a9b407aea033070eea8a
vansoren <jgarciagar@unal.edu.co>
2024-02-20 00:06:32 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,53 @@
+class Auto:
+  cantidadCreados=0
+  def __init__(self,modelo,precio,asientos,marca,motor,registro):
+    self.modelo=modelo
+    self.precio=precio
+    self.asientos=asientos
+    self.marca=marca
+    self.motor=motor
+    self.registro=registro
+
+  def cantidadAsientos(self):
+    a=0
+    for i in self.asientos:
+      if str(type(i))=="<class '__main__.Asiento'>":
+        a+=1
+    return a
+
+  def verificarIdentidad(self):
+    if self.registro==self.motor.registro:
+      for i in self.asientos:
+        if i.registro!=self.registro:
+          return "las piezas no son originales"
+      return "Auto original"
+    else:
+      return "las piezas no son originales"
+
+
+
+class Asiento:
+  colores=["rojo","verde","amarillo","negro","blanco"]
+  def __init__(self,color,precio,registro):
+    self.color=color
+    self.precio=precio
+    self.registro=registro
+
+  def cambiarColor(self,color):
+    if color in self.colores:
+      self.color=color
+
+
+
+class Motor:
+  def __init__(self,numeroCilindros,tipo,registro):
+    self.numeroCilindros=numeroCilindros
+    self.tipo=tipo
+    self.registro=registro
+
+  def cambiarRegistro(self,registro):
+    self.registro=registro
+
+  def asignarTipo(self,tipo):
+    if tipo=="electrico" or tipo=="gasolina":
+      self.motor.tipo=tipo
